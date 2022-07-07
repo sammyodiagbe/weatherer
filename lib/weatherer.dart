a5c4ffb721a7750af4022ae4294693730ee58dcf
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-07 12:37:30 -0500

    working with custom clippaths

--- lib/weatherer.dart
+++ lib/weatherer.dart
@@ -5,14 +5,44 @@ import "package:flutter/material.dart";
 class Weatherer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Container(
+        height: screenHeight,
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/rainy_weather.jpg'),
-                fit: BoxFit.cover)),
-        child: Column(
-          children: [],
+          image: DecorationImage(
+            image: AssetImage('images/sunny_weather.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.4),
+              BlendMode.darken,
+            ),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 20,
+              width: double.infinity,
+            ),
+            // our bottom custom container
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: screenWidth,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  color: Colors.white,
+                ),
+                height: 200,
+              ),
+            )
+          ],
         ),
       ),
     );
