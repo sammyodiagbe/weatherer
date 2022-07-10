fcd26120994437e9838ece0ccb68513b6c39c826
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-10 11:07:21 -0500

    working on the code that gets the user's location

--- lib/models/weatherBrain.dart
+++ lib/models/weatherBrain.dart
@@ -1,5 +1,18 @@
-
-
 // class WeatherBrain {
 //   final Location _location;
 // }
+import 'package:geolocator/geolocator.dart';
+
+class WeatherBrain {
+  // so we want to be able to get the users location
+
+  static void getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (error) {
+      print(error);
+    }
+  }
+}
